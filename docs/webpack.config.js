edefe50a6ba1ea17fc012c61633674717fbf01ac
Cristóvão Trevisan <cristovao.trevisan@gmail.com>
2017-08-01 16:15:06 -0300

    Updating demo site react to production

--- docs/webpack.config.js
+++ docs/webpack.config.js
@@ -30,16 +30,6 @@ module.exports = {
   },
   plugins: [
     new ExtractTextPlugin('bundle.css'),
-    new webpack.optimize.UglifyJsPlugin({
-      beautify: false,
-      mangle: {
-        screw_ie8: true,
-        keep_fnames: true
-      },
-      compress: {
-        screw_ie8: true
-      },
-      comments: false
-    })
+    new webpack.optimize.UglifyJsPlugin()
   ]
 }
